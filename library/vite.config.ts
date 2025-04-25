96f60a99e9fb7cf98a3c81e31b543c36afe4b8ac
seunjin <jinseun@gmail.com>
2025-04-25 11:48:39 +0900

    chore: library 초기 설정

---
+++ library/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "ReactDialogManager",
+      fileName: (format) => `react-dialog-manager.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
