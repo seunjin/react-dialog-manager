7b350655b21b09e0f98957d1d939c726dec10cab
seunjin <jinseun@gmail.com>
2025-04-30 12:08:03 +0900

    feat: 사용자가 타입 확장을 할 수 있는 DialogItemCustomFields, DialogOptionCustomFields 추가

--- library/src/index.ts
+++ library/src/index.ts
@@ -1,1 +1,6 @@
 export { useDialogManager } from "./hooks/useDialogManager";
+export type {
+  DialogOptionCustomFields,
+  DialogItemCustomFields,
+} from "./core/types";
+export { dialogStore } from "./core/store";
